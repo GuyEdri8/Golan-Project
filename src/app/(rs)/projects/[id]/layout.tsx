9a589df072182deaa0bf950b741ab18914ed0445
Eldad Shneor <eldadshneor@gmail.com>
2025-02-24 20:19:25 +0200

    Frogot to update globals + package json

--- src/app/(rs)/projects/[id]/layout.tsx
+++ src/app/(rs)/projects/[id]/layout.tsx
@@ -1,94 +1,5 @@
-<<<<<<< HEAD
 'use client'
 import { ProjectProvider } from "@/components/ProjectContext"
-=======
-// 'use client'
-// import { ProjectProvider } from "@/components/ProjectContext"
-// import { ScrollArea } from "@/components/ui/scroll-area";
-// // import { useRouter, useParams, useSearchParams } from "next/navigation"
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-// import { useState } from "react";
-
-
-// export default function DashboardLayout({
-//     children,
-//     header,
-//     dashboard,
-//     settings,
-//     activity,
-//     documents,
-//     sidebar,
-
-
-// }: Readonly<{
-//     children: React.ReactNode
-//     dashboard: React.ReactNode
-//     header: React.ReactNode
-//     settings: React.ReactNode
-//     activity: React.ReactNode
-//     documents: React.ReactNode
-//     sidebar: React.ReactNode
-
-
-
-
-// }>) {
-//     // const router = useRouter();
-//     // const params = useParams();
-//     // const id = params.id as string;
-//     // const searchParams = useSearchParams();
-//     // const tab = searchParams.get('tab') as string || 'overview';
-//      const [tab, setTab] = useState('overview');
-
-//     return (
-//         <ProjectProvider>
-//             <div className="min-h-screen bg-gray-50">
-
-//                 <div className="container mx-auto px-4 py-8">
-//                     {header}
-//                     <div className="flex flex-col h-full  lg:grid lg:grid-cols-3 mt-8 gap-3">
-//                         <div className="pt-0 flex-1 h-full p-4 rounded-lg lg:col-span-2">
-//                             <Tabs
-//                                 className="flex flex-col justify-start"
-//                                 onValueChange={(value) => {
-//                                     setTab(value);
-//                                 }}
-//                                 defaultValue="overview"
-//                                 dir="rtl"
-
-//                             >
-
-//                             <TabsList>
-//                                 <TabsTrigger value="overview">סקירה כללית</TabsTrigger>
-//                                 <TabsTrigger value="documents">מסמכים</TabsTrigger>
-
-//                                 <TabsTrigger value="activity">יומן פעילות</TabsTrigger>
-//                                 <TabsTrigger value="settings">הגדרות</TabsTrigger>
-//                             </TabsList>
-//                             <TabsContent className='h-full flex-1'  value={tab}>
-//                                 <ScrollArea className="h-full">
-//                                     {tab === 'overview' ? dashboard : tab === 'settings' ? settings : tab === 'activity' ? activity : tab === 'documents' ? documents : children}
-//                                 </ScrollArea>
-//                             </TabsContent>
-//                         </Tabs>
-
-                        
-//                         </div>
-//                         <div className="place-self-center">
-//                             {sidebar}
-//                         </div>
-//                     </div>
-//                 </div>
-
-//             </div>
-//         </ProjectProvider>
-//     )
-// }
-
-'use client'
-import { ProjectProvider } from "@/components/ProjectContext"
-import { ScrollArea } from "@/components/ui/scroll-area";
->>>>>>> guy
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { useState } from "react";
 
@@ -142,11 +53,7 @@ export default function DashboardLayout({
                                 </TabsContent>
                             </Tabs>
                         </div>
-<<<<<<< HEAD
                         <div className="h-full justify-self-end w-full">
-=======
-                        <div className="h-full justify-self-end w-fit">
->>>>>>> guy
                             {sidebar}
                         </div>
                     </div>
--- src/app/(rs)/projects/[id]/page.tsx
+++ src/app/(rs)/projects/[id]/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ProjectDashboard from './@dashboard/page';
 import { getProject } from '@/lib/queries/projects/getProject';
 
@@ -9,41 +8,4 @@ export default async function ProjectDetailsPage({
 }) {
 
   return null;  
-=======
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { TabsContent } from '@/components/ui/tabs';
-import ProjectDashboard from './@dashboard/page';
-import ProjectSettings from './@settings/page';
-
-export default async function ProjectDetailsPage({
-  params,
-  searchParams
-}: {
-  params: { projectId: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
-  const tab = searchParams.tab as string || 'overview';
-
-  if(tab === 'overview') {
-    return (
-      <>
-        <ProjectDashboard />
-      </>
-    )
-  }
-  else if(tab === 'settings') {
-    return (
-      <>
-        <ProjectSettings />
-      </>
-    )
-  }
-
-
-  return (
-    <>
-      <ProjectDashboard />
-    </>
-  )
->>>>>>> guy
 }
